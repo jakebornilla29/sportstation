c74d4cdf6e0b43f4d28b4f386e035935edfcbd8a
Jake Bornilla <37197879+jakebornilla29@users.noreply.github.com>
2018-12-06 23:14:14 +0800

    Initial commit

---
+++ application/controllers/user_controller.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct scripts access allowed');
+
+class user_controller extends CI_Controller{
+
+	public function index()
+	{
+		echo "Welcome User";
+	}
+}
---
+++ application/views/admin/adminn/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="<?php echo base_url('templates/css/bootstrap_login.min.css');?>">
+
+<!-- jQuery library -->
+<script src="<?php echo base_url('templates/css/jquery_login.min.js');?>"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="<?php echo base_url('templates/js/bootstrap_login.min.js')?>"></script>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('templates/css/login.css');?>">
+</head>
+<body>
